ff716fdefe43c98f1521adb198eaf2ddc7818fc3
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-16 22:00:11 -0300

    added project files

---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+  width: 80%;
+
+  h3 {
+    font-family: "Roboto";
+    font-size: 32px;
+    color: #fafafa;
+  }
+
+  p {
+    font-size: 16px;
+    color: #fafafa60;
+    margin-bottom: 20px;
+  }
+
+  a.remover {
+    color: #ff0000;
+    margin-top: 20px;
+  }
+
+  hr {
+    color: #fafafa60;
+    margin-top: 20px;
+  }
+
+  // criando componente de listagem de repositorios 06:00
+`;
